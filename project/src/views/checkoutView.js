dcc4b0c1366f537ad6ba3200565eb670c5181a87
Lothindir <lothindir@gmail.com>
2022-11-21 17:20:26 +0100

    feat: add confirmation and auto fill if user is logged on on checkout

--- project/src/views/checkoutView.js
+++ project/src/views/checkoutView.js
@@ -6,25 +6,14 @@ export default class CheckoutView {
   constructor() {
     this.checkoutEl = document.createElement('div');
     this.checkoutEl.id = 'checkout-container';
-    this.regex = {
-      email: /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/,
-      phone: /^\+?\d{,15}$/,
-    };
-    this.formObject = {
-      firstName: {
-        name: 'firstName',
-        type: 'text',
-        label: 'First Name',
-      }
-    };
   }
 
-  renderContactForm(container, cartData, shipping_fees = 0) {
+  renderContactForm(container, cartData, userData = undefined, shipping_fees = 0) {
     // Render the checkout form
     this.checkoutEl.innerHTML = `
       <div class="checkout-grid">
         <div class="checkout-form-container">
-          ${this.#getContactForm(this.formObject)}
+          ${this.#getContactForm(userData)}
         </div>
         <div id="order-summary">
       </div>
@@ -71,64 +60,39 @@ export default class CheckoutView {
     submitButton.addEventListener('click', () => { container.dispatchEvent(new CustomEvent('placeOrder', { detail: { button: submitButton } })); });
   }
 
-  #validateInput(input, regex) {
-    if (regex.test(input.value)) {
-      input.classList.remove('invalid');
-    }
-    else {
-      input.classList.add('invalid');
-    }
-  }
-
-  #getFormInput(inputObject) {
-    let { name, type, label, invalidMessage, fullSize } = inputObject;
-    return `
-          <div class="checkout-input ${fullSize === true ? 'half' : ''}">
-            <label for="${name}">${label}</label>
-            <div>
-              <input type="${type}" id="${name}" name="${name}"/>
-              ${invalidMessage !== undefined ? `<span class="invalid-message" hidden>${invalidMessage}</span>` : ''}
-            </div>
-          </div>`;
-  }
-
   #getDeliveryOption(deliveryObject, checked = false) {
     return `
-          <label id="shipping-${deliveryObject.id}" class="delivery-card ${checked === true ? 'selected' : ''}">
-            <span>
-              <span>
-                <span class="delivery-method-label">${deliveryObject.method}</span>
-                <span class="delivery-method-description">${deliveryObject.description}</span>
-                <span class="delivery-method-price">NOK ${deliveryObject.price},-</span>
-              </span>
-            </span>
-            <svg
-              class="check-circle"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </label>
-    `;
+      <label id="shipping-${deliveryObject.id}" class="delivery-card ${checked === true ? 'selected' : ''}">
+        <span>
+          <span>
+            <span class="delivery-method-label">${deliveryObject.method}</span>
+            <span class="delivery-method-description">${deliveryObject.description}</span>
+            <span class="delivery-method-price">NOK ${deliveryObject.price},-</span>
+          </span>
+        </span>
+        <svg
+          class="check-circle"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+            clip-rule="evenodd"
+          />
+        </svg>
+      </label>`;
   }
 
-  #getContactForm(formObject) {
-    // return `
-    // <div>
-    //     <h2 class="checkout-title">Shipping information</h2>
+  #getUserData(userData, field, split = -1, delimiter = ' ') {
+    if (userData !== undefined) {
+      return split === -1 ? userData[field] : userData[field].split(delimiter)[split];
+    }
+    return '';
+  }
 
-    //     <div id="checkout-form">
-    //       ${Object.values(formObject).map((input) => this.#getFormInput(input)).join('\n')}
-    //     </div>
-    // </div>
-    // `;
-
+  #getContactForm(userData) {
     return `
       <div>
         <h2 class="checkout-title">Shipping information</h2>
@@ -137,65 +101,42 @@ export default class CheckoutView {
           <div class="checkout-input">
             <label for="first-name">First name</label>
             <div>
-              <input type="text" id="first-name" name="first-name" required />
+              <input type="text" id="first-name" name="first-name" value="${this.#getUserData(userData, 'full_name', 0)}" required />
             </div>
           </div>
 
           <div class="checkout-input">
             <label for="last-name">Last name</label>
             <div>
-              <input type="text" id="last-name" name="last-name" required />
-            </div>
-          </div>
-
-          <div class="checkout-input half">
-            <label for="email-address">Email address</label>
-            <div>
-              <input type="email" id="email-address" name="email-address" required />
-              <span class="invalid-message" hidden>Please enter a valid email address</span>
+              <input type="text" id="last-name" name="last-name" value="${this.#getUserData(userData, 'full_name', 1)}" required />
             </div>
           </div>
 
           <div class="checkout-input half">
             <label for="address">Address</label>
             <div>
-              <input type="text" name="address" id="address" required />
+              <input type="text" name="address" id="address" value="${this.#getUserData(userData, 'street')}" required />
             </div>
           </div>
 
           <div class="checkout-input">
             <label for="city">City</label>
             <div>
-              <input type="text" name="city" id="city" required />
+              <input type="text" name="city" id="city" value="${this.#getUserData(userData, 'city')}" required />
             </div>
           </div>
 
           <div class="checkout-input">
             <label for="postal-code">Postal code</label>
             <div>
-              <input type="text" name="postal-code" id="postal-code" required />
-            </div>
-          </div>
-
-          <div class="checkout-input">
-            <label for="region">State / Province</label>
-            <div>
-              <input type="text" name="region" id="region" required />
-            </div>
-          </div>
-
-          <div class="checkout-input">
-            <label for="country">Country</label>
-            <div>
-              <input type="text" id="country" name="country" required />
+              <input type="text" name="postal-code" id="postal-code" value="${this.#getUserData(userData, 'zipcode')}" required />
             </div>
           </div>
 
           <div class="checkout-input half">
-            <label for="phone">Phone</label>
+            <label for="country">Country</label>
             <div>
-              <input type="text" name="phone" id="phone" required />
-              <span class="invalid-message" hidden>Please enter a valid phone number</span>
+              <input type="text" id="country" name="country" value="${this.#getUserData(userData, 'country')}" required />
             </div>
           </div>
         </form>
@@ -204,13 +145,13 @@ export default class CheckoutView {
 
   #getShippingForm(shippingData) {
     return `
-          <fieldset>
-            <legend class="delivery-methods">Delivery method</legend>
+      <fieldset>
+        <legend class="delivery-methods">Delivery method</legend>
 
-            <div class="delivery-grid">
-              ${shippingData.data.map((delivery) => this.#getDeliveryOption(delivery)).join('\n')}
-            </div>
-          </fieldset>
+        <div class="delivery-grid">
+          ${shippingData.data.map((delivery) => this.#getDeliveryOption(delivery)).join('\n')}
+        </div>
+      </fieldset>
     `;
   }
 }
---
+++ project/src/views/confirmationView.js
@@ -0,0 +1,37 @@
+'use strict';
+
+export default class CheckoutView {
+  renderConfirmation(container, orderData, shippingData) {
+    container.innerHTML = `
+    <div class="confirmation modal">
+      <div id="order-confirmation" class="card">
+        <p>Thank you for your order!</p>
+        <p>Your order number is: ${orderData.record.id}</p>
+        <h3>Customer details</h3>
+        <p>${orderData.record.customer_name}</p>
+        <p>${orderData.record.street}, ${orderData.record.zipcode} ${orderData.record.city}, ${orderData.record.country}</p>
+        <h3>Order details</h3>
+        ${JSON.parse(orderData.record.content).map((product) => `
+          <div class="confirmation-details">
+            <div>${product.name} (${product.id}) - NOK ${product.price},-</div>
+            <div>Quantity: ${product.quantity} - Total NOK ${product.quantity * product.price},-</div>
+          </div>
+        `).join('\n')}
+        <h3>Shipping details</h3>
+        <div class="shipping-details">
+          <div>${shippingData[orderData.record.shipping_id - 1].method}</div>
+          <div>Estimated time delivery: <span>${shippingData[orderData.record.shipping_id - 1].description}</span></div>
+        </div>
+        <h3>Order total</h3>
+        <p>NOK ${JSON.parse(orderData.record.content).reduce((acc, item) => (acc + item.price * item.quantity), 0)},-</p>
+        <button id="back-to-shop" class="btn btn-primary">Back to shop</button>
+      </div>
+    </div>
+    `;
+
+    const backToShopButton = container.querySelector('#back-to-shop');
+    backToShopButton.addEventListener('click', () => {
+      container.dispatchEvent(new CustomEvent('backToShop'));
+    });
+  }
+}
